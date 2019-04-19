688c36131d930659870098d197e4106eea581235
Travis Henson <travisghenson@gmail.com>
2019-04-18 20:54:38 -0400

    Cleaned up and organized code. Began updates to README.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # LIRI App
 LIRI is a Language Interpretation and Recognition Interface. LIRI is a command line Node.js app that takes in parameters and returns data.
+
+## LIRI's Functions
+LIRI has four functions. Three of them take in a user's input, whereas the fourth function is more random and pulls its function parameters from a specified txt file.
+
+The three user-input functions are `concert-this`, `spotify-this-song`, and `movie-this`. The fourth (random function) is `do-what-it-says`.
--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+// ** Requires for the App to Work **
 // Read and set environmental variables with dotenv package
 require("dotenv").config();
 
@@ -17,20 +18,41 @@ var spotify = new Spotify(keys.spotify);
 // Require file-system
 var fs = require("file-system");
 
+// **********************************************************
+
+// ** Main Program Functionality **
 // Determine which command the user has selected
 var selectedCommand = process.argv[2];
 
 // User's input for command argument
 var userArgument = process.argv.splice(3).join(" ");
 
-// Title Case Function
+// Switch statement to run proper function, per user input
+switch (selectedCommand) {
+	case "concert-this":
+		concertThis();
+		break;
+	case "spotify-this-song":
+		spotifyThis();
+		break;
+	case "movie-this":
+		movieThis();
+		break;
+	case "do-what-it-says":
+		doWhat();
+}
+
+// **********************************************************
+
+// ** Function to convert strings to title case **
 function titleCase(string) {
 	return string.replace(/\w\S*/g, function(txt) {
 		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
 	});
 }
 
-// Concert Function
+// ** Base Functions for the LIRI Program **
+// Concert-This Function
 function concertThis(artist) {
 	var artist = userArgument.replace(/\s/g, "");
 	var queryURL = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`;
@@ -41,19 +63,19 @@ function concertThis(artist) {
 			if (response.data[i].venue.region) {
 				region = response.data[i].venue.region + ", ";
 			}
-			console.log(`
+			var concertOutput = `
           Artist: ${titleCase(userArgument)}
           Venue: ${response.data[i].venue.name}
           Location: ${response.data[i].venue.city}, ${region}${
-				response.data[i].venue.country
-			}
+				  response.data[i].venue.country}
           Event Date: ${moment(response.data[i].datetime).format("MM/DD/YYYY")}
-        `);
+        `;
+      console.log(concertOutput);
 		}
 	});
 }
 
-// Spotify Function
+// Spotify-This-Song Function
 function spotifyThis(userSong) {
 	if (userArgument) {
 		userSong = userArgument;
@@ -61,33 +83,37 @@ function spotifyThis(userSong) {
 
 	if (!userSong) {
 		userSong = "The Sign";
-	}
+  }
+  
 	spotify.search({ type: "track", query: userSong }, function(err, data) {
 		if (err) {
-			console.log("Error occurred: " + err);
+      var errorMessage = "Error occurred: " + err;
+      console.log(errorMessage);
 		}
 
 		if (userSong === "The Sign") {
-			console.log(`
+			var defaultSpotifyOutput = `
         Artist: ${data.tracks.items[8].album.artists[0].name}
         Song: ${data.tracks.items[8].name}
         Spotify Preview Link: ${data.tracks.items[8].external_urls.spotify}
         Album: ${data.tracks.items[8].album.name}
-        `);
+        `
+        console.log(defaultSpotifyOutput);
 		} else {
 			for (var i = 0; i <= data.tracks.items.length - 1; i++) {
-				console.log(`
+				var spotifyOutput = `
           Artist: ${data.tracks.items[i].album.artists[0].name}
           Song: ${data.tracks.items[i].name}
           Spotify Preview Link: ${data.tracks.items[i].external_urls.spotify}
           Album: ${data.tracks.items[i].album.name}
-          `);
+          `
+          console.log(spotifyOutput);
 			}
 		}
 	});
 }
 
-// Movie This Function
+// Movie-This Function
 function movieThis(movieTitle) {
 	if (userArgument) {
 		movieTitle = userArgument;
@@ -99,13 +125,14 @@ function movieThis(movieTitle) {
 
 	if (!movieTitle) {
 		movieTitle = "Mr Nobody";
-	}
+  }
+  
 	var movieQuery = movieTitle.replace(/\s+/g, "+");
 	var movieURL = `http://www.omdbapi.com/?t=${movieQuery}&y=&plot=short&apikey=trilogy`;
 
 	axios.get(movieURL).then(function(response) {
 		var movieData = response.data;
-		console.log(`
+		var movieOutput = `
         Title: ${movieData.Title}
         Release Year: ${movieData.Year}
         IMDB Rating: ${movieData.Ratings[0].Value}
@@ -114,11 +141,12 @@ function movieThis(movieTitle) {
         Language: ${movieData.Language}
         Plot: ${movieData.Plot}
         Cast: ${movieData.Actors}
-        `);
+        `;
+    console.log(movieOutput);
 	});
 }
 
-// Do what it says Function
+// Do-What-It-Says Function
 function doWhat() {
 	fs.readFile("random.txt", "utf8", function(error, data) {
 		if (error) {
@@ -141,17 +169,4 @@ function doWhat() {
 		}
 	});
 }
-// Switch statement to run proper function, per user input
-switch (selectedCommand) {
-	case "concert-this":
-		concertThis();
-		break;
-	case "spotify-this-song":
-		spotifyThis();
-		break;
-	case "movie-this":
-		movieThis();
-		break;
-	case "do-what-it-says":
-		doWhat();
-}
+
